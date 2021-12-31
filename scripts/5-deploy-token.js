01ca630d0b1f063daf1a24aa8977555dc34aa2f2
ryvince <90051936+ryvince@users.noreply.github.com>
2021-12-30 20:09:56 -0600

    later updates

--- scripts/5-deploy-token.js
+++ scripts/5-deploy-token.js
@@ -10,7 +10,7 @@ const app = sdk.getAppModule("0x8106Fa831bD3038e4f0160a6DAeB9F6C2e8de953");
       // What's your token's name? Ex. "Ethereum"
       name: "Homeless Governance Token",
       // What's your token's symbol? Ex. "ETH"
-      symbol: "HLGOV",
+      symbol: "HOUSE",
     });
     console.log(
       "✅ Successfully deployed token module, address:",
